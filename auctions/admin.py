a9d447f504e4079f133e324141cf2ea3bfb745d0
SaadHusayn <rsaad5095@gmail.com>
2024-07-03 10:36:15 +0500

    Initial Commit

---
+++ auctions/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+
+admin.site.register(ListingInformation)
+admin.site.register(User)
+admin.site.register(Comment)
+admin.site.register(Bid)
---
+++ auctions/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    pass
+
+
+
+class ListingInformation(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField(max_length=200)
+    price = models.FloatField()
+    image = models.ImageField(blank=True, upload_to="photos")
+    category = models.CharField(max_length=64, blank=True)
+    lister = models.ForeignKey(User, on_delete=models.CASCADE , related_name="listings")
+    time = models.DateTimeField()
+    isListingOpen = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.title} listed by {self.lister}"
+
+class Comment(models.Model):
+    content = models.CharField(max_length=120)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE,  related_name="comments")
+    listing = models.ForeignKey(ListingInformation, on_delete=models.CASCADE, related_name="comments")
+    
+    def __str__(self):
+        return f"{self.writer}: {self.content}"
+
+
+
+class Bid(models.Model):
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="biddings")
+    listing = models.ForeignKey(ListingInformation, on_delete=models.CASCADE, related_name="bids")
+    bidPrice = models.FloatField()
+
+    def __str__(self):
+        return f"Bid of {self.price} by {self.bidder} on {self.listing}"
